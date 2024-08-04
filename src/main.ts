435da1e15eaa65129e0fddeb24b191f262b8cd16
vitaliiverdiiev <vitaliiverdiiev@gmail.com>
2024-08-04 17:24:30 +0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cors from 'cors';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3303;
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cors({
+      credentials: true,
+      origin: '*',
+    }),
+  );
   await app.listen(PORT, () => console.log(`Starting app ${PORT}`));
 }
 
